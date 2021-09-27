d63fb8810026b0c2fc51a2cfc451457baf2854aa
JFMai90 <46156907+JFMai90@users.noreply.github.com>
2021-09-27 01:31:16 -0700

    add redux

---
+++ ExpensifyApp/src/playground/destructure.js
@@ -0,0 +1,27 @@
+// const person = {
+//   fName: 'Jay',
+//   old: 30,
+//   location: {
+//     city: 'SF',
+//     temp: 70
+//   }
+// };
+
+// const { fName = 'Anon', old, location } = person;
+// const { city, temp } = person.location;
+
+// console.log(` is ${old} living in ${city} where it is ${temp} degrees.`)
+
+
+const book = {
+  title: 'Ego',
+  author: 'Ryan',
+  publisher: {
+    name: 'Penguin'
+  }
+};
+
+const { title, author, publisher } = book;
+const { name: publisherName = 'Self-Published' } = book.publisher;
+
+console.log(publisherName);
--- ExpensifyApp/src/playground/redux101.js
+++ ExpensifyApp/src/playground/redux101.js
@@ -1,11 +1,57 @@
 //yarn add redux@3.7.2
-
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const addCount = ({ addBy = 1 } = {}) => ({
+  type: 'ADD',
+  addBy
+})
+
+const minusCount = ({ minusBy = 1 } = {}) => ({
+  type: 'MINUS',
+  minusBy
+})
+
+const setCount = ({ setTo = 888 } = {}) => ({
+  type: 'SET',
+  setTo
+})
+
+const reset = () => ({
+  type: 'RESET'
+})
+
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return {
+        count: state.count + action.addBy
+      };
+    case 'MINUS':
+      return {
+        count: state.count - action.minusBy
+      };
+    case 'SET':
+      return {
+        count: action.setTo
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
 });
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+})
 
-console.log(store.getState());
+//dispatch = action
+store.dispatch(addCount({ addBy: 50 }))
+
+store.dispatch(minusCount({ minusBy: 1000 }))
+
+store.dispatch(setCount({ setTo: 666 }))
+
+store.dispatch(reset())
